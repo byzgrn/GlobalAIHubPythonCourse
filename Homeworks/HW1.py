630f5084150819c70dea6223457f15fdd160e521
byzgrn <81137857+byzgrn@users.noreply.github.com>
2021-04-07 01:02:24 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,17 @@
-#Explain your work
+#homework1
 
-#Question 1
-for x in range(a):
-	print(a)
+'''Firstly, i made a 2 lists. One of them has even numbers, one of them has odd 
+numbers.Then, i merged these two lists. In addition, i sorted them.After that, i
+multiplied all values in the list by 2. Finally, i printed the data type of all 
+values in the list.'''
+
+list1=[1,3,5,7,9]
+list2=[2,4,6,8]
+list1.extend(list2)
+print(list1)
+list1.sort()
+print(list1)
+list3=[x*2 for x in list1]
+print(list3)
+for a in list3:
+    print(type(a))
